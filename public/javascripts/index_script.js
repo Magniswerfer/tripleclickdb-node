a4abb44902e76aed224a999701830e1cd40a4e89
Magniswerfer <magn1549@gmail.com>
2022-04-25 10:28:45 -0400

    functional table generator

--- public/javascripts/index_script.js
+++ public/javascripts/index_script.js
@@ -1,10 +1,60 @@
-const gamesTable = document.querySelector(".gamesTable");
+const gamesTableBody = document.querySelector(".gamesTableBody");
 
 let games;
 
 axios.get('/getAllGames').then(resp => {
-    games = resp.data
-}).then(
+    games = resp.data;
+    games.forEach(game => {
+        let newRow = document.createElement("tr")
 
+        // Create first cell in each row, and make it a link to the game page.
+        let col1 = document.createElement('td');
+        let gameLinkElement = document.createElement('a');
+        let linkText = document.createTextNode(game.name);
+        gameLinkElement.appendChild(linkText); 
+        gameLinkElement.title = game.name;
+        gameLinkElement.href = "/games/"+game.id;
+        col1.appendChild(gameLinkElement);
+        
+        let col2 = document.createElement('td');
+        let col3 = document.createElement('td');
 
-);
+        let epName;
+        let epDate;
+        //Most recent mentions
+        axios.get('/getMostRecentMentionsOf/'+game.id).then(resp =>{
+            epName = resp.data.name;
+            epDate = resp.data.date;
+
+            let epYear = epDate.toString().slice(0, 4);
+            let epMonth = epDate.toString().slice(5, 6);
+            let epDay = epDate.toString().slice(7, 8);
+
+            epDate = epYear + "-" + epMonth + "-" + epDay;
+
+            let date = new Date (epDate)
+            
+            col2.innerHTML = epName;
+            col3.innerHTML = date.toLocaleDateString('en', { year:"numeric", month:"short", day:"numeric"}) ;
+        
+        });
+        
+        let col4 = document.createElement('td');
+        axios.get('/getMentionsOf/'+game.id).then(resp =>{
+            mentions = resp.data.length
+            col4.innerHTML = mentions;
+        });
+
+        //Create the rest of the cells... Implement later lolz
+        
+        
+        //Append all cells to the row
+        newRow.appendChild(col1);
+        newRow.appendChild(col2);
+        newRow.appendChild(col3);
+        newRow.appendChild(col4);
+
+        // Then append the row to the table
+        gamesTableBody.appendChild(newRow);
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
+const res = require('express/lib/response');
 var router = express.Router();
 
 const db = require('../database-functions');
 
+
 function getAllGames(req, res, next) {
   db.findAllGames().then(games => {
-    console.log(games);
     res.data = games;
     next();
   }).catch(err => {
@@ -13,13 +14,49 @@ function getAllGames(req, res, next) {
 });
 }
 
+function getMentionsOf(req, res, next){
+  db.findMentionsWhereGameis(req.params.id).then(mentions => {
+    res.data = mentions;
+    next();
+  }).catch(err => {
+    next(err);
+});
+}
+
+function getMostRecentEp(req, res, next){
+  let allMentions = JSON.parse(res.data);
+    
+  let highestEpID = 0;
+  allMentions.forEach(mention => {
+    console.log("Mention: ")
+    console.log(mention);
+    if(mention.id > highestEpID){
+      highestEpID = mention.episode;
+      console.log("highed ep id now: " + highestEpID)
+    }
+  });
+
+  db.findEpByPrimaryKey(highestEpID).then(ep =>{
+    res.ep = ep;
+    console.log(res.test);
+    next();
+  })
+}
+
 router.get('/getAllGames', getAllGames, function(req, res){
-  res.end(res.data);
+  res.send(res.data);
 });
 
+router.get('/getMentionsOf/:id', getMentionsOf, function(req, res){
+  res.send(res.data);
+});
+
+router.get('/getMostRecentMentionsOf/:id', [getMentionsOf, getMostRecentEp], function(req, res){
+  res.send(res.ep);
+});
 
 /* GET home page. */
-router.get('/', /*getAllGames,*/ function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'TripleClickDB' });
 });
 
